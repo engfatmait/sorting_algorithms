a807da588cc572731899439de73206b87d3cfa92
engfatmait <eng_fatma_it@yahoo.com>
2024-05-04 10:54:38 -0700

    try

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef _sort_
-#define _sort_
+#ifndef _SORT_
+#define _SORT_
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -17,8 +17,20 @@ typedef struct listint_s
 	struct listint_s *prev;
 	struct listint_s *next;
 } listint_t;
-void bubble_sort(int *array, size_t size);
+
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void _qsort(int *a, int low, int high, int size);
+void shell_sort(int *array, size_t size);
+void _ssort(int *a, int size, int n);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
 #endif
